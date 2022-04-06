18b487ddc9fea74e957b8a4d60af80b2f5216cd6
SuyundukovDenisDmitrievich <Denis.dmitrievichLHS@yandex.ru>
2022-04-06 21:30:51 +0300

    Исправил задания и добавил кавычки  как отдельный элемент

--- lesson_2/DZ_2.py
+++ lesson_2/DZ_2.py
@@ -18,21 +18,27 @@ lst_str = ['в', '5', 'часов', '17', 'минут', 'температура'
 # создаю новый список
 lst_str_copy = []
 # создаю переменную для склейки строк
-separator = " "
+separator = ""
 
 
 for el in lst_str:
     # проверяю есть ли в строке цифра
     if el.isdigit():
         # добовляю элемент обособив его кавычками и добовляю знак 0 перед цифрой
-        lst_str_copy.append(f'"{el.zfill(2)}"')
+        lst_str_copy.append('"')
+        lst_str_copy.append(el.zfill(2))
+        lst_str_copy.append('"')
     # проверяю есть ли перед цифрой знак (+ или -)
     elif el.startswith("+") or el.startswith("-"):
         # добовляю к списку элемент обособив кавычками и добавив знак 0 перед цифрой
-        lst_str_copy.append(f'"{el.zfill(3)}"')
+        lst_str_copy.append('"')
+        lst_str_copy.append(el.zfill(3))
+        lst_str_copy.append('"')
     else:
         # добовляю элемент не являющися цифрой и не имеющий знак (+ или -)
+        lst_str_copy.append(" ")
         lst_str_copy.append(el)
+        lst_str_copy.append(" ")
 
 # вывожу список на экран преобразованый в строку с заранее назначеным разделителем
 print(separator.join(lst_str_copy))
--- lesson_2/DZ_3.py
+++ lesson_2/DZ_3.py
@@ -2,24 +2,31 @@
 # чем может сначала показаться.
 
 lst_str = ['в', '5', 'часов', '17', 'минут', 'температура', 'воздуха', 'была', '+5', 'градусов']
-# создаю новый список
-lst_str_copy = []
 # создаю переменную для склейки строк
-separator = " "
-
+separator = ""
+separator_2 = ''
 # делаю цикл для перебора элементов по индексам
-for el in range(len(lst_str) - 1, 0, -1):
+for el in range(len(lst_str) - 1, -1, -1):
     # определяю есть ли цифра в элементе под индексом
     if lst_str[el].isdigit():
         # создаю локальную переменную в которую записываю вырезаный элемент с добавленым 0 спереди
-        temporary_variable = f'"{lst_str.pop(el).zfill(2)}"'
+        temporary_variable = lst_str.pop(el).zfill(2)
         # добавляю изменёный элемент обратно в список на его место
+        lst_str.insert(el, '"')
         lst_str.insert(el, temporary_variable)
+        lst_str.insert(el, '"')
+
     # проверяю есть ли перед цифрой знак (+ или -)
     elif lst_str[el].startswith("+") or lst_str[el].startswith("-"):
         # создаю локальную переменную в которую записываю вырезаный элемент с добавленым 0 спереди
-        temporary_variable = f'"{lst_str.pop(el).zfill(3)}"'
+        lst_str.insert(el + 1, '"')
+        temporary_variable = lst_str.pop(el).zfill(3)
+        lst_str.insert(el, temporary_variable)
+        lst_str.insert(el, '"')
         # добавляю изменёный элемент обратно в список на его место
+    else:
+        # добовляю пробелы в элементы не содержащие цифр для коректного вывода строки
+        temporary_variable = " " + lst_str.pop(el) + " "
         lst_str.insert(el, temporary_variable)
 
 # вывожу список на экран преобразованый в строку с заранее назначеным разделителем
