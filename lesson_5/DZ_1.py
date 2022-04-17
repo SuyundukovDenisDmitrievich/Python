d2dd926ed143691930d53d62eb8b5d2e7f8daf09
SuyundukovDenisDmitrievich <Denis.dmitrievichLHS@yandex.ru>
2022-04-17 10:12:29 +0300

    Выполнил  задания к уроку 5 и загрузил их на git

---
+++ lesson_5/DZ_1.py
@@ -0,0 +1,24 @@
+#
+# 1. Написать генератор нечётных чисел от 1 до n (включительно), используя ключевое слово yield, например:
+# >>> odd_to_15 = odd_nums(15)
+# >>> next(odd_to_15)
+# 1
+# >>> next(odd_to_15)
+# 3
+# ...
+# >>> next(odd_to_15)
+# 15
+# >>> next(odd_to_15)
+# ...StopIteration...
+
+def odd_nums(args: int) -> int:
+    for el in range(1, (args + 1), 2):
+        yield el
+
+
+n = 15
+generator = odd_nums(n)
+
+for _ in range(1, n + 1, 2):
+    print(next(generator))
+# next(generator)  # если раскомментировать, то должно падать в traceback по StopIteration
